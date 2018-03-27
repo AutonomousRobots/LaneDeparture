356de22da5394b75073ab11167913a9216a3d1cb
Greg <gharms@uark.edu>
2018-03-27 16:56:39 -0500

    sifted out lines that are not of interest using their slope. Also sorted lines into right and left lanes. Still need to make a best fit line for each side

--- hough.cpp
+++ hough.cpp
@@ -43,16 +43,40 @@ int main(int argc, char** argv)
         break;
     }
 
+    // get frame dimentions
+    Size size = src.size();
+    int xMidPoint = size.width / 2;
+    int xRightEdge = size.width;
+    int xLeftEdge = 0;
+    
+    // prepare for Hough
     Mat dst, cdst;
     Canny(src, dst, 50, 200, 3);
     cvtColor(dst, cdst, CV_GRAY2BGR);
 
+    // do the Hough
     vector<Vec4i> lines;
     HoughLinesP(dst, lines, 1, CV_PI/180, 50, 50, 10 );
+    
+    // process each line, draw on cdst and find right and left lanes
+    vector<Vec4i> rightLines, leftLines;
     for( size_t i = 0; i < lines.size(); i++ )
     {
       Vec4i l = lines[i];
-      line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+
+      // find slope, sort into left and right sides
+      float slope = ((float)l[3] - (float)l[1]) / ((float)l[2] - (float)l[0]);
+      if (abs(slope) > 0.5)
+      {
+        if (slope <= 0)
+          rightLines.push_back(l);
+        else
+          leftLines.push_back(l);
+
+        // draw lines on cdst
+        line( cdst, Point(l[0], l[1]), Point(l[2], l[3]), Scalar(0,0,255), 3, CV_AA);
+      }
+      //cout << "here's a line: (" << l[0] << "," << l[1] << ")(" << l[3] << "," << l[4] << ")\n";
     }
     imshow("detected lines", cdst);
   
